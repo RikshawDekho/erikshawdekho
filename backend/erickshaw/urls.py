758b8d2d209a9b1685e71e2a389608365539034c
Akram Siddiqui <siddiquiakram84@gmail.com>
2026-03-08 02:04:35 +0530

    add root route

--- backend/erickshaw/urls.py
+++ backend/erickshaw/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("eRickshawDekho Backend Running")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
